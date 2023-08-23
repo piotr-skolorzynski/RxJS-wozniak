e50ebac0d7f15958f748b572b609f1549f77f23f
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-23 14:49:30 +0200

    fromEvent creation function

--- 2. Observable/index.ts
+++ 2. Observable/index.ts
@@ -1,14 +1,1 @@
-import { from } from 'rxjs';
-
-const somePromise = new Promise((resolve, reject) => {
-  // resolve('Resolved!');
-  reject('Rejected!');
-});
-
-const observableFromPromise$ = from(somePromise);
-
-observableFromPromise$.subscribe({
-  next: (value) => console.log(value),
-  error: (err) => console.log('Error: ', err),
-  complete: () => console.log('Completed'),
-});
+const triggerButton = document.querySelector('button#trigger');
