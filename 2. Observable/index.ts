6eed9b03eb8f21a94aa9a2acf571c91812721200
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-23 09:35:51 +0200

    synchronous emission - next notification

--- 2. Observable/index.ts
+++ 2. Observable/index.ts
@@ -3,8 +3,11 @@ import { Observable } from 'rxjs';
 //empty Observable
 const observable$ = new Observable((subscriber) => {
   console.log('Observable executed');
+  subscriber.next('Alice');
 });
 
 console.log('Before subscribe');
-observable$.subscribe();
+observable$.subscribe({
+  next: (value) => console.log(value),
+});
 console.log('After subscribe');
