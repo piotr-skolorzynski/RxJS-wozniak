b6accc97a7634d258216fc69576e00a06ddbff1b
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-23 09:51:28 +0200

    complete observable

--- 2. Observable/index.ts
+++ 2. Observable/index.ts
@@ -1,13 +1,18 @@
 import { Observable } from 'rxjs';
 
-//empty Observable
 const observable$ = new Observable((subscriber) => {
   console.log('Observable executed');
   subscriber.next('Alice');
+  subscriber.next('Ben');
+  setTimeout(() => {
+    subscriber.next('Charlie');
+    subscriber.complete();
+  }, 2000);
 });
 
 console.log('Before subscribe');
 observable$.subscribe({
   next: (value) => console.log(value),
+  complete: () => console.log('Observable emission ended'),
 });
 console.log('After subscribe');
