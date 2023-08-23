2e6dc897221e551c434ed271afa2c5c7ed32bb4b
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-23 15:17:40 +0200

    timer and interval creation function

--- 2. Observable/index.ts
+++ 2. Observable/index.ts
@@ -1,1 +1,13 @@
-const triggerButton = document.querySelector('button#trigger');
+import { timer, interval } from 'rxjs';
+
+timer(2000).subscribe({
+  next: (value) => console.log('Timer: ', value),
+  complete: () => console.log('Timer Completed'),
+});
+
+const subscription = interval(1000).subscribe({
+  next: (value) => console.log('Interval: ', value),
+  complete: () => console.log('Interval Completed'),
+});
+
+setTimeout(() => subscription.unsubscribe(), 5000);
