240be16ddeddbf5bd847a06bef8a199c6f3b704e
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-23 08:23:27 +0200

    multiple subscriptions

--- 2. Observable/index.ts
+++ 2. Observable/index.ts
@@ -2,23 +2,17 @@ import { Observable } from 'rxjs';
 
 const observable$ = new Observable<string>((subscriber) => {
   console.log('Observable executed');
-  //observable will emit below 3 values
   subscriber.next('Alice');
   setTimeout(() => subscriber.next('Ben'), 2000);
   setTimeout(() => subscriber.next('Charlie'), 4000);
 });
 
-// const observer = {
-//   next: (value: string) => console.log(value),
-// };
-
-// observable$.subscribe(observer);
-
-const subscription = observable$.subscribe((value: string) =>
-  console.log(value)
-);
+console.log('Subscription 1 starts');
+observable$.subscribe((value) => console.log('Subscription 1: ', value));
 
 setTimeout(() => {
-  subscription.unsubscribe();
-  console.log('Subscription ended');
-}, 3000);
+  console.log('Subscription 2 starts');
+  observable$.subscribe((value) =>
+    console.log('Subscription 2: ', value + ' ' + 'mleko')
+  );
+}, 2000);
