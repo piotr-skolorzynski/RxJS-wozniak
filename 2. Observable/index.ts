68c5eb87a14c3140723eb96fb0e0ff87e8077cd0
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-24 13:04:27 +0200

    debounceTime operator

--- 2. Observable/index.ts
+++ 2. Observable/index.ts
@@ -1,11 +1,10 @@
-import { filter, map, of, tap } from 'rxjs';
+import { fromEvent, map, debounceTime } from 'rxjs';
 
-of(1, 7, 3, 6, 2)
+const sliderInput = document.querySelector('input#slider');
+
+fromEvent(sliderInput, 'input')
   .pipe(
-    filter((value) => value > 5),
-    //operator tap pozwala zobaczyć co się dzieje na poszczególnych etapach
-    tap((value) => console.log('Spy after filter: ', value)),
-    map((value) => value * 2),
-    tap((value) => console.log('Spy after map: ', value))
+    debounceTime(2000),
+    map((event) => event.target['value'])
   )
-  .subscribe((value) => console.log('Output: ', value));
+  .subscribe((value) => console.log(value));
