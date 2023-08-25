af92e4c1f4eeaf91d3d7cdc3e5dc3256159197d8
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-25 10:09:09 +0200

    subject in action

---
+++ 3. Subjects/index.ts
@@ -0,0 +1,3 @@
+const emitButton = document.querySelector('button#emit');
+const inputElement: HTMLInputElement = document.querySelector('#value-input');
+const subscribeButton = document.querySelector('button#subscribe');
